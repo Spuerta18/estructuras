1de2b044b07f6c56ceed6c42f7cd1593f30dcd8d
Spuerta18 <santiagopuerta1996@gmail.com>
2024-09-26 20:16:47 -0500

    implementacion de un recorrido a un arreglo de numeros

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let numero = 21
-let variasCosas = [23, true , 'jaime', function(){}, [], numero]
-let otroArreglo =[]
+// let numero = 21
+// let variasCosas = [23, true , 'jaime', function(){}, [], numero]
+// let otroArreglo =[]
 
 
 
-console.log (variasCosas [0] * variasCosas[5])
-console.log (variasCosas [5])
-console.log (variasCosas [0])
-console.log (variasCosas [2])
-console.log (variasCosas [3])
-console.log (variasCosas [4])
+// console.log (variasCosas [0] * variasCosas[5])
+// console.log (variasCosas [5])
+// console.log (variasCosas [0])
+// console.log (variasCosas [2])
+// console.log (variasCosas [3])
+// console.log (variasCosas [4])
+
+
+
+
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(let index = 0; index < numeros.length;index ++){// length para contar los elemtnos y ++ despues de la varible para sumar de a uno 
+// console.log(numeros[index])
+if (numeros[index] % 2== 0){
+    console.log(numeros[index])
+}
+}
